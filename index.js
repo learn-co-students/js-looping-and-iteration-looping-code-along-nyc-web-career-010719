912f9256baf9aec3af8a22ab3d23793a1fa6599c
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-18 11:48:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function printBadges(array) {
+  for (i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+  };
+  return array;
+};
+
+function tailsNeverFails() {
+  let flip;
+  let i = 0;
+
+  while (flip !== 0) {
+    i++;
+    flip = Math.round(Math.random());
+  };
+
+  return `You got ${i - 1} tails in a row!`
+};
