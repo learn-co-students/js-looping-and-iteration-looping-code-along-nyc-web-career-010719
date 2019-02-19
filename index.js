7a2db440766755c5798e56aed5c41d393d622a84
powell956 <powell956@gmail.com>
2019-02-19 03:50:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solutions in this file
+// function printBadges(employees) {
+//   for (let i = 0; i < employees.length; i++) {
+//     console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+//   }
+//   return employees;
+// }
+//
+// function coinFlip() {
+//   let flip = Math.random();
+//
+//   if flip >= 0.5 {
+//     return "tails";
+//   } else {
+//     return "heads";
+//   }
+// }
+//
+// function tailsNeverFails() {
+//   let roundCount = 0;
+//   let roundResult = " ";
+//
+//
+//   do {
+//     roundResult = coinFlip();
+//     roundCount++;
+//   } while roundResult === "tails";
+//
+//   return `You got ${roundCount} tails in a row!`;
+// }
+
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
